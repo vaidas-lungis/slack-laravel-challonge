645ccd5b72f442af9387f5dc38f16d874f14ea96
vaidas-lungis <vaidas.lungis+github@gmail.com>
2017-11-27 23:37:07 +0200

    Create heroku database connection

--- config/database.php
+++ config/database.php
@@ -78,6 +78,16 @@ return [
             'prefix' => '',
         ],
 
+        'heroku' => [
+            'driver'   => 'mysql',
+            'host'     => parse_url(env("DATABASE_URL"))["host"],
+            'database' => substr(parse_url(env("DATABASE_URL"))["path"], 1),
+            'username' => parse_url(env("DATABASE_URL"))["user"],
+            'password' => parse_url(env("DATABASE_URL"))["pass"],
+            'charset'  => 'utf8',
+            'prefix'   => '',
+        ],
+
     ],
 
     /*
